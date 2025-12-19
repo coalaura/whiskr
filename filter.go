3b50b1d8bf2d5f6d5ac20c5580d88129c10bcf74
Laura <laura@wiese2.org>
2025-12-19 04:25:31 +0100

    cleanup and fix filters

--- filter.go
+++ filter.go
@@ -1,27 +1,34 @@
 package main
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/expr-lang/expr"
 	"github.com/expr-lang/expr/vm"
 )
 
+type FilterPatcher struct{}
+
 type Filters struct {
 	program *vm.Program
 }
 
 type FilterModel struct {
-	Slug  string  `expr:"slug"`
-	Name  string  `expr:"name"`
-	Price float64 `expr:"price"`
+	Slug    string   `expr:"slug"`
+	Name    string   `expr:"name"`
+	Price   float64  `expr:"price"`
+	Tags    []string `expr:"tags"`
+	Created int64    `expr:"created"`
 }
 
 func (f *Filters) Match(md *Model) (bool, error) {
 	match, err := expr.Run(f.program, FilterModel{
-		Slug:  md.ID,
-		Name:  md.Name,
-		Price: max(md.Pricing.Input, md.Pricing.Output),
+		Slug:    md.ID,
+		Name:    md.Name,
+		Price:   max(md.Pricing.Input, md.Pricing.Output),
+		Tags:    md.Tags,
+		Created: md.Created,
 	})
 
 	if err != nil {
@@ -36,24 +43,44 @@ func ParseFilters(query string) (*Filters, error) {
 		return nil, nil
 	}
 
-	query = strings.ReplaceAll(query, "~", " contains ")
-	query = strings.ReplaceAll(query, "^", " has_prefix ")
-	query = strings.ReplaceAll(query, "$", " has_suffix ")
+	containsRgx := regexp.MustCompile(`(\w+)\s*~\s*('[^']+'|"[^"]+"|\w+)`)
+	query = containsRgx.ReplaceAllString(query, "_contains($1, $2)")
+
+	prefixRgx := regexp.MustCompile(`(\w+)\s*\^\s*('[^']+'|"[^"]+"|\w+)`)
+	query = prefixRgx.ReplaceAllString(query, "_has_prefix($1, $2)")
+
+	suffixRgx := regexp.MustCompile(`(\w+)\s*\$\s*('[^']+'|"[^"]+"|\w+)`)
+	query = suffixRgx.ReplaceAllString(query, "_has_suffix($1, $2)")
 
 	options := []expr.Option{
 		expr.AsBool(),
 		expr.Env(FilterModel{}),
 
-		expr.Function("contains",
+		expr.Function("_contains",
 			func(params ...any) (any, error) {
-				p1 := strings.ToLower(params[0].(string))
-				p2 := strings.ToLower(params[1].(string))
+				search := strings.ToLower(params[1].(string))
 
-				return strings.Contains(p1, p2), nil
+				switch val := params[0].(type) {
+				case string:
+					val = strings.ToLower(val)
+
+					return strings.Contains(val, search), nil
+				case []string:
+					for _, entry := range val {
+						if strings.ToLower(entry) == search {
+							return true, nil
+						}
+					}
+
+					return false, nil
+				}
+
+				return false, nil
 			},
-			new(func(string, string) bool),
+			new(func(any, string) bool),
 		),
-		expr.Function("has_prefix",
+
+		expr.Function("_has_prefix",
 			func(params ...any) (any, error) {
 				p1 := strings.ToLower(params[0].(string))
 				p2 := strings.ToLower(params[1].(string))
@@ -62,7 +89,8 @@ func ParseFilters(query string) (*Filters, error) {
 			},
 			new(func(string, string) bool),
 		),
-		expr.Function("has_suffix",
+
+		expr.Function("_has_suffix",
 			func(params ...any) (any, error) {
 				p1 := strings.ToLower(params[0].(string))
 				p2 := strings.ToLower(params[1].(string))
