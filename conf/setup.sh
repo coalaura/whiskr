ee94c2028e95570ab5c24c9c674c3e3ff259b87b
Laura <laura@wiese2.org>
2025-12-24 01:18:21 +0100

    small cleanup

--- conf/setup.sh
+++ conf/setup.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 
+# --- HARDWARE PERMISSIONS ---
+# If this service requires hardware access, you likely need a udev rule
+# to assign ownership to the 'whiskr' user.
+# Example: /etc/udev/rules.d/99-whiskr.rules
+# SUBSYSTEM=="usb", ATTRS{idVendor}=="XXXX", OWNER="whiskr"
+# ----------------------------
+
 echo "Linking sysusers config..."
 
 mkdir -p /etc/sysusers.d
@@ -13,9 +20,11 @@ fi
 ln -s "/var/wskr.sh/conf/whiskr.conf" /etc/sysusers.d/whiskr.conf
 
 echo "Creating user..."
+
 systemd-sysusers
 
 echo "Linking unit..."
+
 if [ -f /etc/systemd/system/whiskr.service ]; then
     rm /etc/systemd/system/whiskr.service
 fi
@@ -23,10 +32,12 @@ fi
 systemctl link "/var/wskr.sh/conf/whiskr.service"
 
 echo "Reloading daemon..."
+
 systemctl daemon-reload
 systemctl enable whiskr
 
 echo "Fixing initial permissions..."
+
 chown -R whiskr:whiskr "/var/wskr.sh"
 
 find "/var/wskr.sh" -type d -exec chmod 755 {} +
@@ -36,6 +47,6 @@ chmod +x "/var/wskr.sh/whiskr"
 
 echo "Setup complete, starting service..."
 
-service whiskr start
+service whiskr restart
 
 echo "Done."
