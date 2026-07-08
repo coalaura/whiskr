8f94da4fa27df3f5121f09fd03e52f5a1d299ca4
Laura <laura@wiese2.org>
2026-07-09 00:47:14 +0200

    improve rsbuild config

--- static/rsbuild.config.js
+++ static/rsbuild.config.js
@@ -1,19 +1,40 @@
 import { defineConfig } from "@rsbuild/core";
 
-export default defineConfig({
-	html: {
-		template: "./src/index.html",
-	},
-	source: {
-		entry: {
-			index: "./src/js/chat.js",
+export default defineConfig(({ command }) => {
+	const isProd = command === "build";
+
+	return {
+		dev: {
+			hmr: false,
+			liveReload: false,
 		},
-	},
-	output: {
-		distPath: {
-			root: "dist",
-			css: "css",
-			js: "js",
+		html: {
+			template: "./src/index.html",
+			minify: isProd,
 		},
-	},
+		source: {
+			entry: {
+				index: "./src/js/chat.js",
+			},
+		},
+		output: {
+			distPath: {
+				root: "dist",
+				css: "css",
+				js: "js",
+			},
+			cleanDistPath: true,
+			sourcemap: !isProd,
+			minify: isProd
+				? {
+						js: { compress: { drop_console: true } },
+						css: true,
+					}
+				: false,
+			chunkSplit: {
+				strategy: "split-by-size",
+				minSize: 40 * 1024,
+			},
+		},
+	};
 });
